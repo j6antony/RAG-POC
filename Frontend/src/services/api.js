5319520456af77e1ab770ddab60bd82d1403ee92
Johan Antony <j6antony@uwaterloo.ca>
2026-09-17 17:47:29 -0400

    working version

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 // Contract: askQuestion(question) -> { answer, sources: [{ title, section, text }] }
 export async function askQuestion(question) {
-  const response = await fetch('http://127.0.0.1:8000', {
+  const response = await fetch('http://127.0.0.1:8000/chat', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
